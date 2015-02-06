a5d5216548ed18c5a4fb83d1f5e7afab0e624bef
Tiffany Chiou <tiffanichiou@gmail.com>
2015-02-05 22:35:06 -0800

    Debugged some session issues in content1.php and created the content2.php file

--- content1.php
+++ content1.php
@@ -4,7 +4,20 @@ ini_set('display_errors', 1);
 
 session_start();
 $login_url = "login.php";
- 
+
+if ((isset($_GET['logout'])) && ($_GET['logout']==1)) {
+  $_SESSION = array();
+  session_destroy();
+  header('Location: login.php', true);
+  die();
+}
+
+if (!isset($_POST['username'])) {
+  header('Location: login.php', true);
+}
+//  if the POST array has no assignment set for username, it will redirect back to the login page
+// this is different from having an empty string; redirect will not happen then
+
 if (empty($_POST['username'])) {
   echo "A username must be entered. Click <a href=\"$login_url\">here</a> to return to the login screen.";
 }
@@ -15,7 +28,7 @@ else {
     $_SESSION['counts']=0;
   }
 
-  echo "Hello $_SESSION[username] you have visited this page $_SESSION[counts] times before. Click <a href=\"login.php?logout=1\">here</a> to logout.";
+  echo "Hello $_SESSION[username] you have visited this page $_SESSION[counts] times before. Click <a href=\"content1.php?logout=1\">here</a> to logout.";
   
   $_SESSION['counts']++;
 
